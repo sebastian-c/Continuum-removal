f33733ff2f7619cd022f4949d414523780e0b6b7
Sebastian <sebastian.campbell@sydney.edu.au>
2012-06-07 16:09:34 +1000

    Finished convex hull output, graphs as of yet unfinished

--- Fan Convex Hull Spectra.r
+++ Fan Convex Hull Spectra.r
@@ -1,45 +1,91 @@
+####Instructions####
+#Define parameters in the 'Parameter setup' section
+#
 #####Parameter setup#####
 #INSERT PARAMETERS YOU WISH TO SET HERE
-
 #FOR ALL DIRECTORIES, USE '/' instead of '\'
 
-#Set working directory
+#Set working directory and destination directory
 wd <- "C:/Research/Fan Deng" #Set this to spectra location
 
-#Set destination directory
 dest_wd <- wd  #replace right side of arrow with proposed destination in quotes. 
                #defaults to spectra location
 
-#Set spectra filename
+#Set input and output spectra filename
 specname <- "for FanSpectra.txt"
+outname <- "hull-removed spectra.txt"
 
 #Set boundaries
-max_wav <- 350:2500 #Set minimum and maximum for your spectra
-int_wav <- 350:2500 #Set the wavelengths in which you are interested
+max_wav <- 419:2500 #Set minimum and maximum for your spectra
+int_wav <- 419:2500 #Set the wavelengths in which you are interested
 
-setwd("C:/Research/Fan Deng")
+#Do you want graphs?
+wantgraphs <- FALSE
 
-source("Convex hull.r")
+#MAGIC HAPPENS HERE:
 
-fan_spectra <- read.table("for FanSpectra.txt", header=T)
+####Actual code####
 
-hull_list <- list()
-
-for(i in 1:nrow(fan_spectra)){
-  hull_list[[i]] <- c_hull_deviation(fan_spectra[i,], .interval = 419:2500, .all = 419:2500, .return_hull = T)
-}
-
-#slightly slower implementation, but with progress bar
+setwd(wd)
 library(plyr)
 
-fast_hull_list <- alply(fan_spectra, 1, c_hull_deviation, .interval = 419:2500, .all = 419:2500, .return_hull = T)#, .progress="text")
+#Define function determining spectra in interval
+in_interval <- function(.all,.interval,...){
+  ## index for an interval
+  ## a function to subset a particular waveband interval
+  .in_interval = .all %in% .interval
+}
 
-##loop:
-# user  system elapsed 
-# 36.11    0.01   36.24 
+#Define convex hull function
+c_hull_deviation <- function(.spectra, .all = 350:2500, .interval = 350:2500, .return_hull = F,...){
+  ## a function to perform deviations from the convex hull
+  ## at the moment only tested on reflectance values
+  ## not absorbance
+  ##
+  ## organize data
+  ##
+  ## get the interval
+  .in_interval <- in_interval(.all = .all, .interval = .interval)
+  ##
+  ## sort the data
+  .data <- sortedXyData(.all[.in_interval], .spectra[.in_interval])
+  ##
+  ## calculate convex hull
+  c_hull <- chull(.data)
+  ##
+  ## get the approprite region
+  c_hull <- c_hull[which(c_hull == 1):length(c_hull)]
+  ##
+  ## calculate linear approximation between hull points
+  linear_approx <- approx(.data[c_hull,], xout = .interval, method = 'linear', ties = 'mean')
+  ##
+  ## calculate the deviation from the convex hull
+  deviation <- ( linear_approx[[2]] - .spectra[.in_interval] )/linear_approx[[2]]
+  ##
+  ## add the hull if you wish
+  if(.return_hull == T){attr(deviation, 'hull') <-linear_approx[[2]]}
+  ##
+  ## return
+  return(deviation)
+  ##
+}
 
-##plyr function without progress bar
-# user  system elapsed 
-# 35.69    0.00   35.82
-##with progress bar
-# 36.55    0.03   36.53
+#Define graphing function
+spec_graphs <- function(.specvec, .hullvec){
+  
+}
+
+fan_spectra <- read.table(specname, header=T)
+fan_matrix <- as.matrix(fan_spectra)
+dimnames(fan_matrix)[[2]]<-substring(names(fan_spectra),2)
+
+fan_matrix_chull<- aaply(fan_matrix, 1,c_hull_deviation, .all=max_wav, .interval=int_wav, .return_hull=FALSE,
+                         .progress=progress_win(title="Subtracting convex hull..."))
+
+dest_dir <- file.path(dest_wd, outname)
+
+write.table(fan_matrix_chull, dest_dir)
+
+if(wantgraphs) 
+###
+
